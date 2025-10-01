f51b63d9e318f957072d7732a4247ec1a4b6e259
shing <shing1223@yahoo.com.hk>
2025-10-01 23:55:34 +0800

    .

--- components/CollapsibleHeader.tsx
+++ components/CollapsibleHeader.tsx
@@ -109,7 +109,6 @@ export default function CollapsibleHeader({
             <span className={`align-middle inline-block mx-1 h-[14px] w-[52px] rounded-sm ${activeBg}`} />
             {brand}
           </span>
-          <span className="text-xs text-gray-500 dark:text-gray-400">{handle}</span>
         </div>
         <div className="flex items-center gap-2">
           <Link
